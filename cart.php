a87c251d3ebd6212325351504d13f6a5a1d451d6
SHIRISH <shirish.shrestha07@gmail.com>
2024-03-11 09:52:29 +0545

    complete: cart page

--- cart.php
+++ cart.php
@@ -1,5 +1,25 @@
 <?php
 @include 'config.php';
+
+if(isset($_POST['update_update_btn'])){
+   $update_value = $_POST['update_quantity'];
+   $update_id = $_POST['update_quantity_id'];
+   $update_query = mysqli_query($conn, "UPDATE cart SET quantity='$update_value' WHERE id='$update_id' ");
+   if($update_query){
+      header('location:cart.php');
+   }
+}
+
+if(isset($_GET['remove'])){
+   $remove_id=$_GET['remove'];
+   mysqli_query($conn,"DELETE from cart where id ='$remove_id'");
+   header('location:cart.php');
+}
+
+if(isset($_GET['delete_all'])){
+   mysqli_query($conn,"DELETE from cart");
+   header('location:cart.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,10 +46,14 @@
       </thead>
       <tbody>
        <?php
-      
+         $select_cart = mysqli_query($conn, "SELECT * FROM cart");
+         $grand_total = 0;
+         if(mysqli_num_rows($select_cart) > 0){
+            while($fetch_cart = mysqli_fetch_assoc($select_cart)){
+         
        ?>
          <tr>
-            <td><img src="uploaded_img/<?php echo $fetch_cart['image']; ?>" height="100" alt=""></td>
+            <td><img src="<?php echo UPLOAD_DIR,$fetch_cart['image']; ?>" height="100" alt=""></td>
             <td><?php echo $fetch_cart['name']; ?></td>
             <td>Rs.<?php echo number_format($fetch_cart['price']); ?>/-</td>
             <td>
@@ -43,6 +67,8 @@
             <td><a href="cart.php?remove=<?php echo $fetch_cart['id']; ?>" onclick="return confirm('remove item from cart?')" class="delete-btn"> <i class="fas fa-trash"></i> remove</a></td>
          </tr>
          <?php
+         $grand_total = $sub_total;
+            }}
          ?>
          <tr class="table-bottom">
             <td><a href="display.php" class="option-btn" style="margin-top: 0;">continue shopping</a></td>
@@ -57,6 +83,5 @@
    </div>
 </section>
 </div>
-<script src="script.js"></script>
 </body>
 </html>
